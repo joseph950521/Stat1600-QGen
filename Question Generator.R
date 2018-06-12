9ceadf2fc7979eb05cbb72f8d1c2d1e51ff2e743
IKapenga <ian.r.kapenga@wmich.edu>
2018-06-12 14:49:39 -0400

    Added FallaciesMC1 and ConfoundersMC1 qgens. First test bank is complete!

--- Question Generator.R
+++ Question Generator.R
@@ -8,7 +8,7 @@
 ##### code and occasionally re-uploads the test banks. The longest part of the  #####
 ##### process is waiting on the e-learning uploads of the images and CSVs.      #####                                                               #####
 
-##### 23/25 Generators for test 1 completed  #####
+##### 25/25 Generators for test 1 completed  #####
 ##### 0/25 Generators for test 2 completed  #####
 ##### 0/25 Generators for test 3 completed  #####
 
@@ -1321,6 +1321,78 @@ type <- "MC"
 answers <- 5
 points.per.q <- 4
 difficulty <- 1
+quest.txt1 <- "A team of researchers "
+quest.txt2 <- " What type of study is this?"
+study1 <- "randomly separates their study's participants into two groups, giving one group a placebo and the other a new treatment to be tested. As the treatment is very experimental, both participants and researchers know whether a specific participant is receiving the new treatment or not."
+study2 <- "randomly separates their study's participants into two groups, giving one group a placebo and the other a new treatment to be tested. As the treatment is not experimental, both participants and researchers do not know whether a specific participant is receiving the new treatment or not."
+study3 <- "randomly separates their study's participants into two groups, giving one group a placebo and the other a new treatment to be tested. As the treatment is somewhat experimental, participants do not know whether they receive the placebo but researchers do know whether a specific participant receives the placebo."
+study4 <- "studies a population of people who lost weight, asking each person in a sample from this population whether they were on a diet and, if so, which diet was it?"
+study5 <- "studies whether hormone therapy affects menstrual cycles in women. All women in the sample under study were monitored for 2 months without hormone therapy, monitored for 2 more months while being given hormone therapy A, and monitored for 2 more months (for 6 months total) while being given hormone therapy B. Each woman thus acted as her own control. At the end of the 6 months, all participants' menstrual cycles were compared."
+study6 <- "studies the effect of a new pesticide on two randomly selected samples from a species of crop-destroying beetles. One group of beetles receives a standard pesticide, and the other receives the new pesticide. Neither the beetles nor the researchers know which pesticide is which, and survival rates for each group are compared throughout administration of several doses."
+study7 <- "studies the effect of a new medicine meant to treat the common cold. Two randomly selected groups of people are chosen for the study; one group receives a standard cold medication while the other gets the new medication. Due to an error in study design, the researchers are aware of which treatment each participant is receiving, although the participants themselves are unaware whether they received the standard or new medications."
+study8 <- "conducts a study of a new medicine meant to treat the common cold. Two randomly selected groups of people are chosen for the study; one group receives a standard cold medication while the other gets the new medication. Due to an error in study design, both the study's participants and the researchers are aware of which treatment each participant is receiving."
+study9 <- "conducts a preliminary study of a population of people who are suffering from Alzheimer's disease. The researchers are interested in learning whether potassium intake plays a role in contracting Alzheimer's. Study participants are asked, when possible, about their long-term dietary habits; of particular interest is whether participants regularly consumed foods or supplements that contain potassium such as bananas."
+study10 <- "conducts a study of a typical course of treatment for recurring pneumonia that involves a sequence of three medications A, B, and C. The sequence is of interest, so subjects are given the sequence of medications A, B, then C on the first occurence of pneumonia, then subjects are given the sequence C, B, then A on the second occurence, etc. All sequences are given to each subject, so that each subject acts as their own control while comparing the effects of the different sequences on pneumonia."
+dat.size = 
+digits = 
+loc.path <- 
+e.path <- 
+hint <- "The important concepts here are from the Study Designs chapter of your coursepack."
+feedback <- "RCT: randomization, control group. Single-Blinding: researchers aware, subjects unaware. Double-Blinding: both researchers and patients unaware. Non-Blnded: Neither patients nor researchers aware. Case-Control: Looks back from outcome to treatment, no randomization. Case-Crossover: Participants act as own control group."
+param <- c("NewQuestion","ID","Title","QuestionText","Points","Difficulty",
+           rep("Option", answers),"Hint","Feedback")
+questions <- data.frame()
+for(i in 1:n)
+{
+  ID <- paste(title, i, sep = "-")
+  points <- sample(c(rep(0,answers-1),100),replace=F)
+  corr.ind <- 6 + which.max(points)
+  data1 <- sample(c(study1, study2, study3, study4, study5,
+                    study6, study7, study8, study9), size = 1)
+  corr.ans <- if(data1 == study1 | data1 == study8){"Non-Blinded Randomized Controlled Trial"}
+              else{if(data1 == study2 | data1 == study6){"Double-Blinded Randomized Controlled Trial"}
+                   else{if(data1 == study3 | data1 == study7){"Single-Blinded Randomized Controlled Trial"}
+                        else{if(data1 == study4 | data1 == study9){"Case-Control Study"}
+                             else{if(data1 == study5 | data1 == study10){"Case-Crossover Study"}}}}}
+  ans.txt <- if(corr.ans == "Non-Blinded Randomized Controlled Trial")
+               {sample(c("Double-Blinded Randomized Controlled Trial", "Single-Blinded Randomized Controlled Trial",
+                         "Case-Control Study", "Case-Crossover Study", "Type 1 Study", "Type 2 Study", "Standard Deviation Study"),
+                       size = answers)}
+             else{if(corr.ans == "Double-Blinded Randomized Controlled Trial")
+                    {sample(c("Non-Blinded Randomized Controlled Trial", "Single-Blinded Randomized Controlled Trial",
+                              "Case-Control Study", "Case-Crossover Study", "Type 1 Study", "Type 2 Study", "Standard Deviation Study"),
+                            size = answers)}
+                  else{if(corr.ans == "Single-Blinded Randomized Controlled Trial")
+                         {sample(c("Non-Blinded Randomized Controlled Trial", "Double-Blinded Randomized Controlled Trial",
+                                   "Case-Control Study", "Case-Crossover Study", "Type 1 Study", "Type 2 Study", "Standard Deviation Study"),
+                                 size = answers)}
+                       else{if(corr.ans == "Case-Control Study")
+                              {sample(c("Non-Blinded Randomized Controlled Trial", "Single-Blinded Randomized Controlled Trial",
+                                        "Double-Blinded Randomized Controlled Trial", "Case-Crossover Study", "Type 1 Study", "Type 2 Study", "Standard Deviation Study"),
+                                      size = answers)}
+                            else{if(corr.ans == "Case-Crossover Study")
+                                   {sample(c("Non-Blinded Randomized Controlled Trial", "Single-Blinded Randomized Controlled Trial",
+                                             "Case-Control Study", "Double-Blinded Randomized Controlled Trial", "Type 1 Study", "Type 2 Study", "Standard Deviation Study"),
+                                           size = answers)}}}}}
+  content <- c(type, ID, ID, paste(quest.txt1, data1, quest.txt2, collapse = "", sep = ""),
+               points.per.q, difficulty, points, hint, feedback)
+  options <- c(rep("",6), ans.txt, rep("",2))
+  options[corr.ind] <- corr.ans
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),1] <- param
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),2] <- content
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),3] <- options
+}
+questions <- questions[((9+answers)):((8+answers)*(n+1)),]
+write.table(questions, sep=",", file=paste(title, ".csv", sep = ""),
+            row.names=F, col.names=F)
+
+##### FallaciesMC1 #####
+title <- "FallaciesMC1"
+n = 200
+type <- "MC"
+answers <- 5
+points.per.q <- 4
+difficulty <- 1
 quest.txt1 <- "While watching a debate online, you listen as one of the debaters claims, '"
 quest.txt2 <- "' This person has just... "
 listen1 <- "There is no hard evidence that she stole the money, thus we must conclude that she did not steal the money."
@@ -1349,27 +1421,97 @@ for(i in 1:n)
   data1 <- sample(c(listen1, listen2, listen3, listen4, listen5,
                     listen6, listen7, listen8, listen9), size = 1)
   corr.ans <- if(data1 == listen1 | data1 == listen2 | data1 == listen3){"... Committed the Lack of Evidence Fallacy"}
-              else{if(data1 == listen4 | data1 == listen5 | data1 == listen6){"... Committed the Anecdotal Evidence Fallacy"}
-                   else{if(data1 == listen7 | data1 == listen8 | data1 == listen9){"... Committed the Correlation Equals Causation Fallacy"}}}
+  else{if(data1 == listen4 | data1 == listen5 | data1 == listen6){"... Committed the Anecdotal Evidence Fallacy"}
+    else{if(data1 == listen7 | data1 == listen8 | data1 == listen9){"... Committed the Correlation Equals Causation Fallacy"}}}
   ans.txt <- if(corr.ans == "... Committed the Lack of Evidence Fallacy")
-               {sample(c("... Conceptualized a Study's Problem", "... Designed a Study",
-                         "... Committed the Anecdotal Evidence Fallacy", "Operationalized a Study's Problem",
-                         "... Committed the Correlation Equals Causation Fallacy", "... Collected Data for a Study",
-                         "... Committed a Type 1 Error", "... Committed a Type 2 Error"),
-                       size = answers)}
-             else{if(corr.ans == "... Committed the Anecdotal Evidence Fallacy")
-                    {sample(c("... Conceptualized a Study's Problem", "... Designed a Study",
-                              "... Committed the Lack of Evidence Fallacy", "... Operationalized a Study's Problem",
-                              "... Committed the Correlation Equals Causation Fallacy", "... Collected Data for a Study",
-                              "... Committed a Type 1 Error", "... Committed a Type 2 Error"),
-                       size = answers)}
-                  else{if(corr.ans == "... Committed the Correlation Equals Causation Fallacy")
-                      {sample(c("... Conceptualized a Study's Problem", "... Designed a Study",
-                                "... Committed the Anecdotal Evidence Fallacy", "... Operationalized a Study's Problem",
-                                "... Committed the Lack of Evidence Fallacy", "... Collected Data for a Study",
-                                "... Committed a Type 1 Error", "... Committed a Type 2 Error"),
-                          size = answers)}}}
+  {sample(c("... Conceptualized a Study's Problem", "... Designed a Study",
+            "... Committed the Anecdotal Evidence Fallacy", "Operationalized a Study's Problem",
+            "... Committed the Correlation Equals Causation Fallacy", "... Collected Data for a Study",
+            "... Committed a Type 1 Error", "... Committed a Type 2 Error"),
+          size = answers)}
+  else{if(corr.ans == "... Committed the Anecdotal Evidence Fallacy")
+  {sample(c("... Conceptualized a Study's Problem", "... Designed a Study",
+            "... Committed the Lack of Evidence Fallacy", "... Operationalized a Study's Problem",
+            "... Committed the Correlation Equals Causation Fallacy", "... Collected Data for a Study",
+            "... Committed a Type 1 Error", "... Committed a Type 2 Error"),
+          size = answers)}
+    else{if(corr.ans == "... Committed the Correlation Equals Causation Fallacy")
+    {sample(c("... Conceptualized a Study's Problem", "... Designed a Study",
+              "... Committed the Anecdotal Evidence Fallacy", "... Operationalized a Study's Problem",
+              "... Committed the Lack of Evidence Fallacy", "... Collected Data for a Study",
+              "... Committed a Type 1 Error", "... Committed a Type 2 Error"),
+            size = answers)}}}
   content <- c(type, ID, ID, paste(quest.txt1, data1, quest.txt2, collapse = "", sep = ""),
+               points.per.q, difficulty, points, hint, feedback)
+  options <- c(rep("",6), ans.txt, rep("",2))
+  options[corr.ind] <- corr.ans
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),1] <- param
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),2] <- content
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),3] <- options
+}
+questions <- questions[((9+answers)):((8+answers)*(n+1)),]
+write.table(questions, sep=",", file=paste(title, ".csv", sep = ""),
+            row.names=F, col.names=F)
+
+##### ConfoundersMC1 #####
+title <- "ConfoundersMC1"
+n = 200
+type <- "MC"
+answers <- 5
+points.per.q <- 4
+difficulty <- 1
+quest.txt1 <- "Some student research assistants are helping study "
+quest.txt2 <- ". The study primarily involves three variables. First, the "
+quest.txt3 <- " sometimes leads to a change in a second variable, the "
+quest.txt4 <- ". However, sometimes a third variable, the "
+quest.txt5 <- " (which is associated with the "
+quest.txt6 <- " and causes changes in the "
+quest.txt7 <- ") interferes with the result. In statistics, what do we call the "
+quest.txt8 <- "?"
+example1 <- c("flowers", "flowers' petal length", "flowers' reproductive success", "flowers' height")
+example2 <- c("other students' academic success", "amount of time students spend studying", "students' GPAs", "students' work schedules")
+example3 <- c("beehives", "number of bees in the hive", "survival of the beehive through winter", "availability of nectar in summer")
+example4 <- c("the student debt problem", "amount of debt accrued while in school", "amount of debt remaining ten years later", "choice of major")
+example5 <- c("mental health", "amount of time a patient spends outside", "state of a patients' depression", "patients' occupation")
+dat.size = 
+digits = 
+loc.path <- 
+e.path <- 
+hint <- ""
+feedback <- ""
+param <- c("NewQuestion","ID","Title","QuestionText","Points","Difficulty",
+           rep("Option", answers),"Hint","Feedback")
+questions <- data.frame()
+for(i in 1:n)
+{
+  ID <- paste(title, i, sep = "-")
+  points <- sample(c(rep(0,answers-1),100),replace=F)
+  corr.ind <- 6 + which.max(points)
+  decis1 <- sample(1:5, size = 1)
+  data <- get(paste("example", decis1, sep = ""))
+  decis2 <- sample(2:4, size = 1)
+  corr.ans <- if(decis2 == 2){"The Probable Cause"}
+              else{if(decis2 == 3){"The Outcome/Effect"}
+                   else{if(decis2 == 4){"The Confounder"}}}
+  ans.txt <- if(decis2 == 2){c(sample(c("The Outcome/Effect", "The Confounder",
+                                        "The Standard Deviation", "The Fallacy"),
+                                      size = answers-1),
+                               sample(c("All of the Above", "None of the Above"),
+                                      size = 1))}
+             else{if(decis2 == 3){c(sample(c("The Confounder", "The Standard Deviation",
+                                             "The Probable Cause", "The Fallacy"),
+                                           size = answers-1),
+                                    sample(c("All of the Above", "None of the Above"),
+                                           size = 1))}
+                  else{if(decis2 == 4){c(sample(c("The Outcome/Effect", "The Standard Deviation",
+                                                  "The Probable Cause", "The Fallacy"),
+                                                size = answers-1),
+                                         sample(c("All of the Above", "None of the Above"),
+                                                size = 1))}}}
+  content <- c(type, ID, ID, paste(quest.txt1, data[1], quest.txt2, data[2], quest.txt3, data[3],
+                                   quest.txt4, data[4], quest.txt5, data[2], quest.txt6, data[3],
+                                   quest.txt7, data[decis2], quest.txt8,
+                                   collapse = "", sep = ""),
                points.per.q, difficulty, points, hint, feedback)
   options <- c(rep("",6), ans.txt, rep("",2))
   options[corr.ind] <- corr.ans
