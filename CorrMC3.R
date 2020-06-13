59a45856e7897bd36f800415db8de6bb4a5b5908
MohammadAbrar07 <66123774+MohammadNahian@users.noreply.github.com>
2020-06-13 15:20:16 -0400

    R question generating files regarding 'Correlation'

---
+++ CorrMC3.R
@@ -0,0 +1,45 @@
+##### CorrMC3 #####
+CorrMC3= function(
+title = "CorrMC3", # Question-bank title that will be easily viewable in e-learning
+n = 200, # Number of questions to generate
+type = "MC", # The question type, one of many possible types on e-learning
+answers = 5, # Number of answers per MC question
+points.per.q = 4, # Number of points the question is worth (on the test)
+difficulty = 1, # An easily viewable difficulty level on e-learning
+quest.txt1 = "A researcher compares the correlation coefficients between six pairs of variables. She calculates them as follows: ",
+quest.txt2 = ". Which of the correlations is the weakest?", # the above two texts are static text for the question
+dat.size = 1, # This is the number of values to be randomly generated for the dataset
+digits = 2, # This is the number of decimal places to round off the data
+loc.path , # This is the local path used to store any randomly generated image files
+  e.path , # This is the path on e-learning used to store any above-implemented image files
+  hint = "This is an interpretive question. No calculation is necessary.",
+feedback = "Stronger correlations are closer to -1 or 1. Weaker correlations are closer to 0." # This is student feedback, visible after the exam
+)
+{
+param <- c("NewQuestion","ID","Title","QuestionText","Points","Difficulty",
+           rep("Option", answers),"Hint","Feedback") # These are setting row names for the CSV file
+questions <- data.frame() # This opens a data frame to store the randomly generated questions below
+for(i in 1:n)
+{
+  ID <- paste(title, i, sep = "-") # The ID of the specific question within the bank, title + question number in the loop
+  points <- sample(c(rep(0,answers-1),100),replace=F) # The proportion of points assigned to each possible answer, 1 if correct or 0 if incorrect
+  corr.ind <- 6 + which.max(points) # This is the row index of the correct answer
+  r <- sample(seq(-1,1,10^-digits), size = 6) # randomly generating correlation values
+  data1 <- paste(r[1], r[2], r[3], r[4], r[5], r[6], sep = ", ") # publishing the correlation values inside the question
+  corr.ans <- r[which.min(abs(r))] # This is the correct answer to the question
+  ans.txt <- r[-which.min(abs(r))] # These are the incorrect answers
+  content <- c(type, ID, ID, paste(quest.txt1, data1, quest.txt2,
+                                   collapse = "", sep= ""),
+               points.per.q, difficulty, points, hint, feedback) # This is collecting a lot of the above information into a single vector
+  options <- c(rep("",6), ans.txt, rep("",2)) # This is collecting the incorrect answers above, and indexing them correctly by row
+  options[corr.ind] <- corr.ans # This is imputing the correct answer at the appropriate row index
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),1] <- param # This is indexing and storing all the row names
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),2] <- content # Indexing and storing all the content
+  questions[(1+(8+answers)*i):((8+answers)*(i+1)),3] <- options # Indexing and storing the answers, both incorrect and correct
+}
+questions <- questions[(9+answers):((8+answers)*(n+1)),] # Storing only what's needed for e-learning upload as a CSV file
+write.table(questions, sep=",", file=paste(title, ".csv", sep = ""),
+            row.names=F, col.names=F) # Writing the CSV file
+}
+
+CorrMC3() # creating the csv file
